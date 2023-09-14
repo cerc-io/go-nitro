6a4ac5623d6da04a9b6a5cf3bf237109958d459f
neeraj <neeraj.rtly@gmail.com>
2023-09-14 16:47:58 +0530

    Revert "Use mplex muxer (#4)"
    This reverts commit 0472b96da88b619501e3cceaf369431a69825657.

--- node/engine/messageservice/p2p-message-service/service.go
+++ node/engine/messageservice/p2p-message-service/service.go
@@ -17,7 +17,6 @@ import (
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/protocol"
-	"github.com/libp2p/go-libp2p/p2p/muxer/mplex"
 	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
 	"github.com/libp2p/go-libp2p/p2p/transport/websocket"
 	"github.com/multiformats/go-multiaddr"
@@ -95,7 +94,7 @@ func NewMessageService(ip string, tcpPort int, wsPort int, me types.Address, pk 
 		libp2p.EnableNATService(),
 		libp2p.Transport(websocket.New),
 		// libp2p.NoSecurity, // Use default security options (Noise + TLS)
-		libp2p.Muxer(mplex.ID, mplex.DefaultTransport),
+		libp2p.DefaultMuxers,
 	}
 	host, err := libp2p.New(options...)
 	if err != nil {

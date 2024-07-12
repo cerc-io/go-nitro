4b8fef85f1d4f55e964e6e769ac2857c7b945436
Shreerang Kale <shreerangkale@gmail.com>
2024-07-12 11:51:53 +0530

    Parse initial token supply when taken from env variables

--- packages/nitro-protocol/hardhat-deploy/01-token.ts
+++ packages/nitro-protocol/hardhat-deploy/01-token.ts
@@ -17,7 +17,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const name = process.env.TOKEN_NAME || DEFAULT_TOKEN_NAME;
   const symbol = process.env.TOKEN_SYMBOL || DEFAULT_TOKEN_SYMBOL;
-  const initialSupply = process.env.INITIAL_TOKEN_SUPPLY || DEFAULT_INITIAL_TOKEN_SUPPLY;
+  const initialSupply = process.env.INITIAL_TOKEN_SUPPLY
+    ? hre.ethers.utils.parseUnits(process.env.INITIAL_TOKEN_SUPPLY, 18)
+    : DEFAULT_INITIAL_TOKEN_SUPPLY;
 
   await deploy('Token', {
     from: deployer,
